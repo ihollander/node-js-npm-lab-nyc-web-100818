45a23cb9d969a8a82ec598600f4e6d7bff277155
ihollander <ianholla@gmail.com>
2019-02-05 11:18:30 -0500

    Done.

--- test/greet_test.js
+++ test/greet_test.js
@@ -1,17 +1,18 @@
-const expect = require('chai').expect;
-const greet = require('../lib/greet');
+const expect = require("chai").expect;
+const greet = require("../lib/greet");
 
-describe('greet()', () => {
-  it('handles all arguments at once', () => {
-    expect(greet('Hi there', 'you')).to.equal('Hi there, you!');
+console.log(greet);
+describe("greet()", () => {
+  it("handles all arguments at once", () => {
+    expect(greet("Hi there", "you")).to.equal("Hi there, you!");
   });
 
-  it('handles arguments separately', () => {
-    const curried = greet('Yo');
+  it("handles arguments separately", () => {
+    const curried = greet("Yo");
 
-    expect(curried).to.be.a('function');
+    expect(curried).to.be.a("function");
 
-    expect(curried('you')).to.equal('Yo, you!');
-    expect(curried('friend')).to.equal('Yo, friend!');
+    expect(curried("you")).to.equal("Yo, you!");
+    expect(curried("friend")).to.equal("Yo, friend!");
   });
 });
